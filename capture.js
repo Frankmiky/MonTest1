97eebe2dbdaa118f4c17fa4fb5559decfdf5ca0d
Frankmiky <franksimus@yahoo.fr>
2013-08-13 16:52:53 +0200

    Update capture.js

--- capture.js
+++ capture.js
@@ -16,7 +16,6 @@
     function onDeviceReady()
     {
     	$(document).ready(function(){
-    		alert('Jquery Mobie Loaded!');
         });
         //menuDiv = document.querySelector("#footernav2");
         document.addEventListener("online", onOnline, false);
@@ -156,7 +155,6 @@
     // Exit for the App
     function exit()
     {
-       alert('Exit!!');
        navigator.app.exitApp();
     }
     /*/Test :upload a file on a remote Serve PHP server

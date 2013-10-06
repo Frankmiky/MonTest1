6d299a7ccc58f646c30647232a6f2d70bbfcfba4
Frankmiky <franksimus@yahoo.fr>
2013-10-06 13:47:12 +0200

    Update capture.js

--- capture.js
+++ capture.js
@@ -94,7 +94,7 @@
 	function downloadReady()
 	{ 
 		// $("#status").html("Ready to check remote files...");
-		$.get("http://192.168.0.25/download.php", {},picturesArray, "json");
+		$.get("http://192.168.0.14/download.php", {},picturesArray, "json");
 		
 		function picturesArray(imgs)
 		{ 
@@ -109,7 +109,7 @@
 						var fileName = imgs[i].substr(imgs[i].lastIndexOf('/')+1);
 						var ft = new FileTransfer();
 						var dlPath = picturesStore.fullPath + "/" + fileName; 
-						var uri = encodeURI("http://131.246.37.167/uploads/"+ escape(fileName));
+						var uri = encodeURI("http://192.168.0.14/uploads/"+ escape(fileName));
 						ft.download(uri, dlPath, function(entry){
 						console.log("Successful download");
 						}, onReadfail);
@@ -259,7 +259,7 @@
 		options.chunkedMode = false;
 		
 		var ft = new FileTransfer();
-		ft.upload(imageURI, "http://192.168.0.25/upload.php", win, fail, options,true);
+		ft.upload(imageURI, "http://192.168.0.14/upload.php", win, fail, options,true);
 	}
 	 
 	function win(r) 
